cccb4f92244209c5e06a89a210b4ffafa28886c0
wesleymp <wesleyxd@hotmail.com>
2022-02-11 01:21:40 -0300

    refactor: modificada a conexão com o pg, se a variável NODE_ENV for igual a test muda para o bando de dados de teste

--- src/models/connection.ts
+++ src/models/connection.ts
@@ -3,6 +3,14 @@ import dotev from 'dotenv';
 
 dotev.config();
 
+const database = {
+  host: process.env.DATABASE_HOST,
+};
+
+if (process.env.NODE_ENV === 'test') {
+  database.host = process.env.DATABASE_HOST_TEST;
+}
+
 export const connection = new Pool({
-  connectionString: process.env.DATABASE_HOST,
+  connectionString: database.host,
 });
