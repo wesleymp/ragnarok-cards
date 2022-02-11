c4c2f4bad9a28e2a6f0a7b59c197fdf0c7c65af3
wesleymp <wesleyxd@hotmail.com>
2022-02-11 20:18:16 -0300

    test: implementado o teste para verificar se o nome possui mais de 2 caracteres

--- src/middlewares/validateNameMiddleware.ts
+++ src/middlewares/validateNameMiddleware.ts
@@ -2,6 +2,11 @@ import { Request, Response, NextFunction } from 'express';
 
 export const validateNameMiddleware = (req: Request, res: Response, next: NextFunction) => {
   const { name } = req.body;
-  if (!name) return res.status(400).json({ message: 'O campo nome não pode ser nulo.' });
+  if (!name) {
+    return res.status(400).json({ message: 'O campo nome não pode ser nulo.' });
+  }
+  if (name.length < 2) {
+    return res.status(400).json({ message: 'O campo nome deve ter mais de 2 caracteres.' });
+  }
   return next();
 };
--- test/signup.test.ts
+++ test/signup.test.ts
@@ -10,7 +10,18 @@ describe('Testando a rota /signup', () => {
     };
     request(app)
       .post('/signup')
-      .set('Accept', 'application/json')
+      .send(body)
+      .expect(400, done);
+  });
+
+  it('deve retornar um erro 400 caso o campo nome tiver menos de 2 caracteres', (done) => {
+    const body = {
+      name: '1',
+      email: 'valid_email@mail.com',
+      password: 'valid_password',
+    };
+    request(app)
+      .post('/signup')
       .send(body)
       .expect(400, done);
   });
