ffc6bebfa2b92c6d6ff433b2db5f8876041060d2
wesleymp <wesleyxd@hotmail.com>
2022-02-11 20:22:21 -0300

    refactor: modificado o signupService, movida a funcionalidade de checar email da função de signup

--- src/services/signupService.ts
+++ src/services/signupService.ts
@@ -2,9 +2,15 @@ import { error } from './helpers/error';
 import { signupModel } from '../models/signupModel';
 import { signinModel } from '../models/signinModel';
 
+const checkEmail = async (email: string) => {
+  await signinModel(email);
+  if ((await signinModel(email)).rowCount > 0) {
+    throw error(409, 'Este email já foi cadastrado.');
+  }
+};
+
 export const signupService = async (name: string, email: string, password: string) => {
-  const checkEmail = await signinModel(email);
-  if (checkEmail.rowCount > 0) throw error(409, 'Este email já foi cadastrado.');
+  await checkEmail(email);
   await signupModel(name, email, password);
   return { status: 201, message: 'Registrado com sucesso!' };
 };
