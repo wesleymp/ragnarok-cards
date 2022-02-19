90933f39ff02216bf784f6cecf6e8300ec34c3ef
wesleymp <wesleyxd@hotmail.com>
2022-02-19 16:36:22 -0300

    test: criado o teste de integração da rota /all-cards

---
+++ test/integration/allCards.test.ts
@@ -0,0 +1,62 @@
+import request from 'supertest';
+
+import app from '../../src/main/app';
+import { connection } from '../../src/models/connection';
+import { genereteJwt } from '../../src/util/jwt';
+
+describe('Testando a rota /all-cards', () => {
+  const data = {
+    id: 1,
+    name: 'valid_name',
+    email: 'valid_email@mail.com',
+    role: 'admin',
+  };
+
+  const token = `Bearer ${genereteJwt(data)}`;
+
+  afterEach(async () => {
+    const conn = await connection.connect();
+    await conn.query('DELETE FROM "cards"');
+  });
+
+  it('deve retornar um status 401 se o usuário não informar um token para poder adicionar um novo card', (done) => {
+    const headers = {
+      authorization: '',
+    };
+    request(app)
+      .get('/all-cards')
+      .set(headers)
+      .expect(401, done);
+  });
+
+  it('deve retornar um status 401 se o usuário informar um token inválido para poder adicionar um novo card', (done) => {
+    const headers = {
+      authorization: 'invalid_token',
+    };
+    request(app)
+      .get('/all-cards')
+      .set(headers)
+      .expect(401, done);
+  });
+
+  it('deve retornar um status 404 se não encontrar nenhum card', (done) => {
+    const headers = {
+      authorization: token,
+    };
+    request(app)
+      .get('/all-cards')
+      .set(headers)
+      .expect(404, done);
+  });
+
+  it('deve retornar um status 200 se encontrar 1 ou mais cards', (done) => {
+    connection.connect().then((conn) => conn.query('INSERT INTO "cards" (name, image, description) VALUES (\'valid_name\', \'valid_image.png\', \'valid_description\')'));
+    const headers = {
+      authorization: token,
+    };
+    request(app)
+      .get('/all-cards')
+      .set(headers)
+      .expect(200, done);
+  });
+});
