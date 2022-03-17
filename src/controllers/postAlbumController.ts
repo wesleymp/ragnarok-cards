305ade5731f16222dd256eecc7dc53e0ccc5dfdc
wesleymp <wesleyxd@hotmail.com>
2022-03-17 07:30:09 -0300

    fix: modificando o nome da chave do retorno do json

--- src/controllers/postAlbumController.ts
+++ src/controllers/postAlbumController.ts
@@ -5,7 +5,7 @@ import { postAlbumService } from '../services';
 export const albumController = async (req: Request, res: Response) => {
   try {
     const albumData = await postAlbumService(req.body, req.headers);
-    return res.status(albumData.status).json({ data: albumData.message });
+    return res.status(albumData.status).json({ message: albumData.message });
   } catch (error: any) {
     return res.status(error.status).json({ message: error.message });
   }
